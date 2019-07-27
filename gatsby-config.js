ddffd26231fb60f2ce47a3cac131f260cef20b9a
Łukasz Rybka <darkstariw@gmail.com>
2019-07-28 00:26:55 +0200

    Initial Gatsby project.

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "GRAPHCMS",
+        fieldName: "graphCMSData",
+        url: `https://api-euwest.graphcms.com/v1/cjw246tvx0ec901et32e3l0po/master`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-5CFC74P",
+        includeInDevelopment: false,
+      },
+    },
+  ],
+}
