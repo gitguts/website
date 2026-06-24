15391fefae66cead4a04dcd7a52b7feec0785117
Łukasz Rybka <darkstariw@gmail.com>
2026-06-24 21:28:23 +0200

    SEO configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.gitguts.io",
+  integrations: [sitemap()],
+});
